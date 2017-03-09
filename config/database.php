b9be5c41d68b6db131ed290505692dcd24aa930a
Guillermo Puente <guillermo.ps09@gmail.com>
2017-03-09 11:05:15 -0300

    Fixed db database forge for codeship env

--- config/database.php
+++ config/database.php
@@ -68,9 +68,9 @@ return [
 
         'mysql_test' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST_TEST', '127.0.0.1'),
-            'port' => env('DB_PORT_TEST', '3306'),
-            'database' => env('DB_DATABASE_TEST', 'forge'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
             'username' => env('MYSQL_USER',env('DB_USERNAME_TEST', 'forge')),
             'password' => env('MYSQL_PASSWORD',env('DB_PASSWORD_TEST', '')),
             'charset' => 'utf8',
